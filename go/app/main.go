bca576ca3967918453a79640b2e648af7d3a8ed3
nachan28 <torajiro282828@gmail.com>
2023-05-17 12:25:33 +0900

    Implement addItem func

--- go/app/main.go
+++ go/app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
@@ -20,19 +21,51 @@ type Response struct {
 	Message string `json:"message"`
 }
 
+type Item struct {
+	Name     string `json:"name"`
+	Category string `json:"category"`
+}
+
+type ItemWrapper struct {
+	Items []Item `json:"items"`
+}
+
 func root(c echo.Context) error {
 	res := Response{Message: "Hello, world!"}
 	return c.JSON(http.StatusOK, res)
 }
 
+// Create wrapper
+var wrapper ItemWrapper
+
 func addItem(c echo.Context) error {
 	// Get form data
 	name := c.FormValue("name")
-	c.Logger().Infof("Receive item: %s", name)
+	category := c.FormValue("category")
 
-	message := fmt.Sprintf("item received: %s", name)
+	// Create item object
+	item := Item{name, category}
+	c.Logger().Infof("Receive item: %s, category: %s", name, category)
+
+	// Add item to wrapper
+	wrapper.Items = append(wrapper.Items, item)
+
+	// Create items.json
+	file, err := os.OpenFile("items.json", os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatal(err)
+		return c.JSON(http.StatusInternalServerError, Response{Message: "Failed to save item"})
+	}
+	defer file.Close()
+
+	// Write items into items.json
+	encoder := json.NewEncoder(file)
+	if err := encoder.Encode(wrapper); err != nil {
+		log.Fatal(err)
+	}
+	// Return message
+	message := fmt.Sprintf("item received: %s, category: %s", name, category)
 	res := Response{Message: message}
-
 	return c.JSON(http.StatusOK, res)
 }
 
@@ -72,7 +105,6 @@ func main() {
 	e.GET("/", root)
 	e.POST("/items", addItem)
 	e.GET("/image/:imageFilename", getImg)
-
 
 	// Start server
 	e.Logger.Fatal(e.Start(":9000"))
